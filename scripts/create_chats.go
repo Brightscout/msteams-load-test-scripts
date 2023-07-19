dad8834604d46e1fd09fdf45a1639b600fefc0e6
Manoj <manoj@brightscout.com>
2023-07-19 17:51:07 +0530

    [MI-3312] Review fixes: Refactored some code and added error handling somewhere

--- scripts/create_chats.go
+++ scripts/create_chats.go
@@ -30,7 +30,7 @@ func CreateChats(config *serializers.Config, logger *zap.Logger) error {
 	}
 
 	if len(store.Users) >= constants.MinUsersForDM {
-		newDMID, err := getOrCreateChatForUsers(client, []string{store.Users[0].ID, store.Users[1].ID})
+		newDMID, err := getOrCreateChatForUsers(client, []string{store.Users[0].ID, store.Users[1].ID}, logger)
 		if err != nil {
 			logger.Error("unable to create the DM", zap.Error(err))
 		} else {
@@ -45,7 +45,7 @@ func CreateChats(config *serializers.Config, logger *zap.Logger) error {
 			store.Users[0].ID,
 			store.Users[1].ID,
 			store.Users[2].ID,
-		})
+		}, logger)
 		if err != nil {
 			logger.Error("unable to create the GM", zap.Error(err))
 		} else {
@@ -64,20 +64,20 @@ func CreateChats(config *serializers.Config, logger *zap.Logger) error {
 	return nil
 }
 
-func getOrCreateChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string) (string, error) {
+func getOrCreateChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string, logger *zap.Logger) (string, error) {
 	if len(userIDs) == 2 {
-		return createChatForUsers(client, userIDs, models.ONEONONE_CHATTYPE)
+		return createChatForUsers(client, userIDs, models.ONEONONE_CHATTYPE, logger)
 	}
 
-	chatID, err := getChatForUsers(client, userIDs)
+	chatID, err := getChatForUsers(client, userIDs, logger)
 	if err == nil && chatID != "" {
 		return chatID, nil
 	}
 
-	return createChatForUsers(client, userIDs, models.GROUP_CHATTYPE)
+	return createChatForUsers(client, userIDs, models.GROUP_CHATTYPE, logger)
 }
 
-func getChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string) (string, error) {
+func getChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string, logger *zap.Logger) (string, error) {
 	requestParameters := &users.ItemChatsRequestBuilderGetQueryParameters{
 		Select: []string{"members", "id"},
 		Expand: []string{"members"},
@@ -88,6 +88,7 @@ func getChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string) 
 
 	res, err := client.Users().ByUserId(userIDs[0]).Chats().Get(context.Background(), configuration)
 	if err != nil {
+		logger.Error("Unable to get user chats", zap.String("UserID", userIDs[0]), zap.Error(utils.NormalizeGraphAPIError(err)))
 		return "", utils.NormalizeGraphAPIError(err)
 	}
 
@@ -96,8 +97,13 @@ func getChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string) 
 			matches := map[string]bool{}
 			for _, m := range c.GetMembers() {
 				for _, u := range userIDs {
-					userID, err2 := m.GetBackingStore().Get("userId")
-					if err2 == nil && userID != nil && *(userID.(*string)) == u {
+					userID, err := m.GetBackingStore().Get("userId")
+					if err != nil {
+						logger.Error("Error in getting user ID from chat member", zap.String("MemberDisplayName", *m.GetDisplayName()), zap.Error(err))
+						continue
+					}
+
+					if *(userID.(*string)) == u {
 						matches[u] = true
 						break
 					}
@@ -112,7 +118,7 @@ func getChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string) 
 	return "", nil
 }
 
-func createChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string, chatType models.ChatType) (string, error) {
+func createChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []string, chatType models.ChatType, logger *zap.Logger) (string, error) {
 	members := make([]models.ConversationMemberable, len(userIDs))
 	for idx, userID := range userIDs {
 		conversationMember := models.NewConversationMember()
@@ -130,6 +136,7 @@ func createChatForUsers(client *msgraphsdkgo.GraphServiceClient, userIDs []strin
 	chatRequestBody.SetMembers(members)
 	newChat, err := client.Chats().Post(context.Background(), chatRequestBody, nil)
 	if err != nil {
+		logger.Error("Unable to create chat", zap.Error(utils.NormalizeGraphAPIError(err)))
 		return "", utils.NormalizeGraphAPIError(err)
 	}
 
--- serializers/config.go
+++ serializers/config.go
@@ -31,38 +31,38 @@ type ChannelsConfiguration struct {
 }
 
 func (c *Config) IsConnectionConfigurationValid() error {
-	if c.ConnectionConfiguration.TenantID == "" {
-		return errors.New("tenantID should not be empty")
-	}
-
-	if c.ConnectionConfiguration.ClientID == "" {
-		return errors.New("clientID should not be empty")
-	}
-
-	if c.ConnectionConfiguration.ClientSecret == "" {
-		return errors.New("clientSecret should not be empty")
-	}
-
 	config := c.ConnectionConfiguration
 	config.TenantID = strings.TrimSpace(config.TenantID)
 	config.ClientID = strings.TrimSpace(config.ClientID)
 	config.ClientSecret = strings.TrimSpace(config.ClientSecret)
+
+	if config.TenantID == "" {
+		return errors.New("tenantID should not be empty")
+	}
+
+	if config.ClientID == "" {
+		return errors.New("clientID should not be empty")
+	}
+
+	if config.ClientSecret == "" {
+		return errors.New("clientSecret should not be empty")
+	}
 
 	return nil
 }
 
 func (c *Config) IsUsersConfigurationValid() error {
 	for idx, user := range c.UsersConfiguration {
+		user.Email = strings.TrimSpace(user.Email)
+		user.Password = strings.TrimSpace(user.Password)
+
 		if user.Email == "" {
-			return fmt.Errorf("%s. index: %d", "user email should not be empty", idx)
+			return fmt.Errorf("user email should not be empty. index: %d", idx)
 		}
 
 		if user.Password == "" {
-			return fmt.Errorf("%s. index: %d", "user password should not be empty", idx)
+			return fmt.Errorf("user password should not be empty. index: %d", idx)
 		}
-
-		user.Email = strings.TrimSpace(user.Email)
-		user.Password = strings.TrimSpace(user.Password)
 	}
 
 	return nil
@@ -70,28 +70,28 @@ func (c *Config) IsUsersConfigurationValid() error {
 
 func (c *Config) IsChannelsConfigurationValid() error {
 	for idx, channel := range c.ChannelsConfiguration {
-		if channel.TeamID == "" {
-			return fmt.Errorf("%s. index: %d", "team ID should not be empty", idx)
-		}
-
-		if channel.ChannelID == "" {
-			if channel.ChannelDisplayName == "" {
-				return fmt.Errorf("%s. index: %d", "channel display name should not be empty", idx)
-			}
-
-			if channel.Type == "" {
-				return fmt.Errorf("%s. index: %d", "channel type should not be empty", idx)
-			}
-
-			if !strings.EqualFold(channel.Type, "P") && !strings.EqualFold(channel.Type, "O") {
-				return fmt.Errorf("%s. index: %d", `invalid channel type. allowed values are "P" and "O"`, idx)
-			}
-		}
-
 		channel.TeamID = strings.TrimSpace(channel.TeamID)
 		channel.Type = strings.TrimSpace(channel.Type)
 		channel.ChannelID = strings.TrimSpace(channel.ChannelID)
 		channel.ChannelDisplayName = strings.TrimSpace(channel.ChannelDisplayName)
+
+		if channel.TeamID == "" {
+			return fmt.Errorf("team ID should not be empty. index: %d", idx)
+		}
+
+		if channel.ChannelID == "" {
+			if channel.ChannelDisplayName == "" {
+				return fmt.Errorf("channel display name should not be empty. index: %d", idx)
+			}
+
+			if channel.Type == "" {
+				return fmt.Errorf("channel type should not be empty. index: %d", idx)
+			}
+
+			if !strings.EqualFold(channel.Type, "P") && !strings.EqualFold(channel.Type, "O") {
+				return fmt.Errorf("invalid channel type. allowed values are %q and %q. index: %d", "O", "P", idx)
+			}
+		}
 	}
 
 	return nil
